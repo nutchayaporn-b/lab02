05a7f62c8fff76df7fc5d402c9b490c63be3b4de
nutchayaporn-b <nutchayaporn_b@cmu.ac.th>
2022-07-12 00:10:54 +0700

    5.5) Now it shows only id, which has been clicked, try to update only the view to show the number of each product added

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true,
         };
     },
     methods: {
-        addToCart() {
-            this.cart++;
+        updateCart(id) {
+            // Group the cart by id
+            const cartItem = this.cart.find((item) => item.id === id);
+            if (cartItem) {
+                cartItem.quantity++;
+            } else {
+                this.cart.push({
+                    id,
+                    quantity: 1,
+                });
+            }
         },
     },
 });
