f2e23e07773476b6f0282e6a1da698313b060136
nutchayaporn-b <nutchayaporn_b@cmu.ac.th>
2022-07-11 23:05:53 +0700

    2.6) Bind out-of-stock image class to the image whenever instock is false

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
+            product: "Shoes",
+            image: "./assets/images/socks_green.jpg",
+            inStock: false,
             inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
+            details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                {
+                    id: 2234,
+                    color: "green",
+                    image: "./assets/images/socks_green.jpg",
+                },
+                {
+                    id: 2235,
+                    color: "blue",
+                    image: "./assets/images/socks_blue.jpg",
+                },
             ],
-            cart: 0
-
-        }
+            cart: 0,
+        };
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
         updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+            this.image = variantImage;
+        },
+    },
+});
